57ba44e7aaa497f9c280415843d5592f3f270863
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-16 17:28:42 +0100

    dsds

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
 const v1 = require("./routes/v1/indexRoutes");
-
+import cors from "cors";
 //const auth = require("./utils/authentication");
-const cors = require("cors");
-const corOptions = {
+/*const corOptions = {
   origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
-
+*/
 //Conectar
 //rpalrodcommerceapi.onrender.com/v1/api/users
-app.use(cors(corOptions));
+app.use(cors());
 
 app.use(express.json());
 //app.use(auth.login)
