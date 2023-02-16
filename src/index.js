0de74a11af127f617b4fc842abe221ef24184cf8
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-16 16:48:26 +0100

    dsadas

--- src/index.js
+++ src/index.js
@@ -25,6 +25,15 @@ app.use(express.json());
 //app.use(auth.login)
 app.use("/v1/api", v1.router);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`[INFO] Servidor escuchando en ${PORT} `);
