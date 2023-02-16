8e3264b67a736ce84c378f866aea77b894ae07be
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-16 16:28:06 +0100

    dsadas

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 //Conectar
 //rpalrodcommerceapi.onrender.com/v1/api/users
-app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+app.use(cors({ origin: "http://localhost:8080" }));
 app.options("*", cors());
 app.use(express.json());
 //app.use(auth.login)
